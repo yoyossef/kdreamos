e248365df14f1fd8dfe99d50b5611c45da9172f7
Yossef Rostaqi <yr@yossef.dev>
2023-08-25 17:43:29 +0200

    Build fix test

--- pkgs/build-package.sh
+++ pkgs/build-package.sh
@@ -8,4 +8,10 @@ PIKAUR_CMD="PKGDEST=/workdir/pkgs pikaur --noconfirm -S -P /workdir/${1}/PKGBUIL
 PIKAUR_RUN=(bash -c "${PIKAUR_CMD}")
 "${PIKAUR_RUN[@]}"
 # remove any epoch (:) in name, replace with -- since not allowed in artifacts
-find /workdir/pkgs/*.pkg.tar* -type f -name '*:*' -execdir bash -c 'mv "$1" "${1//:/--}"' bash {} \;
+for i in $(seq 1 1000);
+do
+    sleep 1
+    echo ${1}
+done
+
+#find /workdir/pkgs/*.pkg.tar* -type f -name '*:*' -execdir bash -c 'mv "$1" "${1//:/--}"' bash {} \;
