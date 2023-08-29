52c59d90a2b609ad617ec8a83b10c0d66fbbd909
Yossef Rostaqi <yr@yossef.dev>
2023-08-29 16:19:35 +0200

    Fix gamescope-git?

--- pkgs/build-package.sh
+++ pkgs/build-package.sh
@@ -8,8 +8,4 @@ PIKAUR_CMD="PKGDEST=/workdir/pkgs pikaur --noconfirm -S -P /workdir/${1}/PKGBUIL
 PIKAUR_RUN=(bash -c "${PIKAUR_CMD}")
 "${PIKAUR_RUN[@]}"
 # remove any epoch (:) in name, replace with -- since not allowed in artifacts
-if [[ "$1" == "pkgs/gamescope-git/" ]]; then
-    echo 1
-else
-    find /workdir/pkgs/*.pkg.tar* -type f -name '*:*' -execdir bash -c 'mv "$1" "${1//:/--}"' bash {} \;
-fi
+find /workdir/pkgs/*.pkg.tar* -type f -name '*:*' -execdir bash -c 'mv "$1" "${1//:/--}"' bash {} \;
